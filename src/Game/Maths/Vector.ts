0da88f1d4cd4fd7857845ceff25192d0a4264678
metafield <anthony.sylvester99@gmail.com>
2021-10-27 08:03:35 -0700

    refactor

--- src/Game/Maths/Vector.ts
+++ src/Game/Maths/Vector.ts
@@ -1,31 +1,25 @@
 import Vector2d from 'vector2d'
 
-export default class Vector extends Vector2d.Vector {
-  static get ZERO() {
-    return new Vector2d.Vector(0, 0)
-  }
+export function ZERO() {
+  return new Vector2d.Vector(0, 0)
+}
 
-  static get UP() {
-    return new Vector2d.Vector(0, -1)
-  }
+export function UP() {
+  return new Vector2d.Vector(0, -1)
+}
 
-  static get DOWN() {
-    return new Vector2d.Vector(0, 1)
-  }
+export function DOWN() {
+  return new Vector2d.Vector(0, 1)
+}
 
-  static get LEFT() {
-    return new Vector2d.Vector(-1, 0)
-  }
+export function LEFT() {
+  return new Vector2d.Vector(-1, 0)
+}
 
-  static get RIGHT() {
-    return new Vector2d.Vector(-1, 0)
-  }
-
-  static create(x: number, y: number) {
-    return new Vector2d.Vector(x, y)
-  }
+export function RIGHT() {
+  return new Vector2d.Vector(-1, 0)
 }
 
 export function vector(x: number, y: number): Vector2d.Vector {
-  return Vector.create(x, y)
+  return new Vector2d.Vector(x, y)
 }
